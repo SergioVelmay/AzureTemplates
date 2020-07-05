ccd6ae143121c888c25ab06533d23c7bdb37b2ee
Sergio Velasco <sergio.velasco@bravent.net>
2020-07-05 13:36:16 +0200

    Bash script for LAMP 5.6 installation added.

---
+++ LAMP_install_MySQLv5.5_PHPv5.6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# resynchronize package index files
+apt-get -y update
+
+# get database password from parameter
+dbpass = $1
+
+# suppress configuration dialogs
+export DEBIAN_FRONTEND=noninteractive
+
+# set the MySQL 5.5 root password
+echo mysql-server-5.5 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# install LAMP with MySQL 5.5 and PHP 5.6
+apt-get -y install apache2 mysql-server-5.5 php5.6 php5.6-mysql
+
+# write some code to show PHP info
+echo \<center\>\<h1\>Server LAMP PHP 5.6 Test\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
+echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
+
+# restart the Apache server
+apachectl restart
